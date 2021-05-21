47851a19d74273aa4c00b29bd587278145d4b9ea
竹内佑介 <kaidouji85@gmail.com>
2021-05-21 09:58:22 +0900

    jwt認証ミドルウェアの不具合を修正した

--- authnicate-jwt.js
+++ authnicate-jwt.js
@@ -1,10 +1,13 @@
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET_KEY} = require('./jwt-secret-key');
+
 module.exports.authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, accessTokenSecret, (err, user) => {
+    jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
       if (err) {
         return res.sendStatus(403);
       }
